8dd2fb6bbc9be5217f16000085104d772ea00d15
Gowthami <gowthamigujarathi@gmail.com>
2025-06-16 16:09:29 +0530

    Updated routing and auth pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import{BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import store from "./store/store.js";
+import { Toaster } from "./components/ui/toaster.jsx";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -13,6 +14,7 @@ root.render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
+      <Toaster/>
     </Provider>
   </BrowserRouter>
 );
--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,24 +1,61 @@
-import { Link } from "react-router-dom";
+import CommonForm from "@/components/common/form";
+import { registerFormControls } from "@/config";
+import { useToast } from "@/hooks/use-toast";
+import { registerUser } from "@/store/auth-slice";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
-function AuthRegister() {
-  return (
+const initialState={
+    userName:'',
+    email:'',
+    password:''
+}
+
+function AuthRegister(){
+
+    const [formData,setFormData]=useState(initialState)
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
+    const {toast}=useToast()
+
+    function onSubmit(event){
+      event.preventDefault();
+      dispatch(registerUser(formData)).then((data)=>{
+        // console.log(data);
+        if(data?.payload?.success) {
+          toast({
+            title:data?.payload?.message,
+          });
+          navigate('/auth/login');
+        } else{
+          toast({
+            title:data?.payload?.message,
+            variant:"destructive"
+          });
+        }
+      });
+    }
+
+    console.log(formData);
+
+    return (
     <div className="mx-auto w-full max-w-md space-y-6">
-      <div className="text-center">
-        <h1 className="text-3xl font-bold tracking-tight text-foreground">
-          Create new account
-        </h1>
-        <p className="mt-2">
-          Already have an account?
-          <Link
-            className="font-medium ml-2 text-primary hover:underline"
-            to="/auth/login"
-          >
-            Login
-          </Link>
-        </p>
-      </div>
+        <div className="text-center">
+            <h1 className="text-3xl font-bold tracing-tight text-foreground">Create new account</h1>
+            <p className="mt-2" >Already have an account
+            <Link className="font-medium text-primary hover:underline ml-2" to='/auth/login' >Login</Link>
+            </p>
+        </div>
+        <CommonForm
+        formControls={registerFormControls}
+        buttonText={'Sign Up'}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+        />
     </div>
-  );
+    );
 }
 
 export default AuthRegister;
